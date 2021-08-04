bbb60e856add5010809b47a1d50766b5b3adfe4a
ernestpazera <ernestpazera@gmail.com>
2021-08-04 04:34:56 -0500

    i can hover over inventory items

--- tggdhj2/State.InPlay.Inventory.cpp
+++ tggdhj2/State.InPlay.Inventory.cpp
@@ -10,10 +10,12 @@
 #include <format>
 #include "Game.Audio.Mux.h"
 #include "Game.Avatar.h"
+#include "Game.Avatar.Equipment.h"
 #include "Game.Avatar.Items.h"
 #include "Game.Avatar.Position.h"
 #include "Game.Avatar.Statistics.h"
 #include "Game.Avatar.Visits.h"
+#include "Game.EquipSlots.h"
 #include "Game.Items.h"
 #include "Game.Nodes.h"
 #include "Game.Nodes.Items.h"
@@ -30,7 +32,9 @@ namespace state::in_play::Inventory
 	const size_t GRID_COLUMNS = 39;
 	const size_t GRID_ROWS = 21;
 	const int INVENTORY_ROW_OFFSET = 1;
+	const int EQUIPMENT_ROW_OFFSET = 1;
 	const std::string FORMAT_INVENTORY_ITEM_COUNT = "{} (x{})";
+	const std::string FORMAT_EQUIPMENT_SLOT = "{}({})";
 	const std::string AREA_INVENTORY = "Inventory";
 	const std::string AREA_EQUIPMENT = "Equipment";
 	const std::string AREA_GO_BACK = "GoBack";
@@ -43,6 +47,8 @@ namespace state::in_play::Inventory
 
 	static std::map<game::Item, size_t> inventoryItems;
 	static std::optional<int> hoverInventoryItem = std::nullopt;
+	static std::map<game::EquipSlot, game::Item> equipment;
+	static std::optional<int> hoverEquipment = std::nullopt;
 	static bool hoverGoBack = false;
 
 	static void WriteGridText(int column, int row, const std::string& text, const std::string& color, const visuals::HorizontalAlignment& alignment)
@@ -56,11 +62,54 @@ namespace state::in_play::Inventory
 			color,
 			alignment);
 	}
+	static int GetGridCellHeight()
+	{
+		return visuals::SpriteGrid::GetCellHeight(LAYOUT_NAME, SPRITE_GRID);
+	}
 
 	static void UpdateInventoryContents()
 	{
 		inventoryItems = game::avatar::Items::ReadAll();
 	}
+
+	static void UpdateEquipmentContents()
+	{
+		equipment = game::avatar::Equipment::ReadAll();
+	}
+
+	static void RefreshEquipmentContents()
+	{
+		WriteGridText(
+			GRID_COLUMNS, 
+			EQUIPMENT_ROW_OFFSET - 1,
+			"Equipment:",
+			visuals::data::Colors::SUBHEADING,
+			visuals::HorizontalAlignment::RIGHT);
+		int row = 0;
+		for (auto equipped : equipment)
+		{
+			auto itemDescriptor = game::Items::Read(equipped.second);
+			auto equipSlotDescriptor = game::EquipSlots::Read(equipped.first);
+			std::string color = (hoverEquipment.has_value() && hoverEquipment.value() == row) ? (visuals::data::Colors::HOVER) : (visuals::data::Colors::NORMAL);
+			WriteGridText(
+				GRID_COLUMNS,
+				row+EQUIPMENT_ROW_OFFSET,
+				std::format(FORMAT_EQUIPMENT_SLOT, itemDescriptor.name, equipSlotDescriptor.name),
+				color,
+				visuals::HorizontalAlignment::RIGHT);
+			++row;
+		}
+		if (row == 0)
+		{
+			WriteGridText(
+				GRID_COLUMNS, row + EQUIPMENT_ROW_OFFSET,
+				"(nothing)",
+				visuals::data::Colors::DISABLED,
+				visuals::HorizontalAlignment::RIGHT);
+		}
+
+	}
+
 
 	static void RefreshInventoryContents()
 	{
@@ -106,39 +155,90 @@ namespace state::in_play::Inventory
 		visuals::SpriteGrid::Clear(LAYOUT_NAME, SPRITE_GRID);//TODO: put in a clear row?
 		RefreshGoBack();
 		RefreshInventoryContents();
+		RefreshEquipmentContents();
 	}
 
 	static void OnEnter()
 	{
 		game::audio::Mux::Play(game::audio::Mux::Theme::MAIN);
 		UpdateInventoryContents();
+		UpdateEquipmentContents();
 		Refresh();
 	}
 
+	static void HandleGoBackMouseMotion(const common::XY<int>&)
+	{
+		hoverGoBack = true;
+	}
+
+	static void HandleInventoryMouseMotion(const common::XY<int>& xy)
+	{
+		int row = xy.GetY() / GetGridCellHeight();
+		if (row < inventoryItems.size())
+		{
+			hoverInventoryItem = row;
+		}
+	}
+
+	static void HandleEquipmentMouseMotion(const common::XY<int>& xy)
+	{
+		int row = xy.GetY() / GetGridCellHeight();
+		if (row < equipment.size())
+		{
+			hoverEquipment = row;
+		}
+	}
+
+	const std::map<std::string, std::function<void(const common::XY<int>&)>> mouseMotionHandlers =
+	{
+		{AREA_GO_BACK, HandleGoBackMouseMotion},
+		{AREA_EQUIPMENT, HandleEquipmentMouseMotion},
+		{AREA_INVENTORY, HandleInventoryMouseMotion}
+	};
+
 	static void OnMouseMotionInArea(const std::string& areaName, const common::XY<int>& xy)
 	{
-		hoverGoBack = areaName == AREA_GO_BACK;
+		hoverGoBack = false;
+		hoverInventoryItem = std::nullopt;
+		hoverEquipment = std::nullopt;
+		mouseMotionHandlers.find(areaName)->second(xy);
 		Refresh();
-		//if (areaName == AREA_FLOOR)
-		//{
-		//	HandleFloorMouseMotion(xy);
-		//}
 	}
 
 	static void OnMouseMotionOutsideArea(const common::XY<int>& xy)
 	{
 		hoverGoBack = false;
+		hoverInventoryItem = std::nullopt;
+		hoverEquipment = std::nullopt;
 		Refresh();
 	}
 
+	static bool HandleGoBackMouseButtonUp()
+	{
+		application::UIState::Write(::UIState::IN_PLAY_NEXT);
+		return true;
+	}
+
+	static bool HandleInventoryMouseButtonUp()
+	{
+		return false;
+	}
+
+	static bool HandleEquipmentMouseButtonUp()
+	{
+		return false;
+	}
+
+	const std::map<std::string, std::function<bool()>> mouseButtonUpHandlers =
+	{
+		{AREA_GO_BACK, HandleGoBackMouseButtonUp},
+		{AREA_INVENTORY, HandleInventoryMouseButtonUp},
+		{AREA_EQUIPMENT, HandleEquipmentMouseButtonUp},
+	};
+
 	static bool OnMouseButtonUpInArea(const std::string& areaName)
 	{
-		if (areaName == AREA_GO_BACK)
-		{
-			application::UIState::Write(::UIState::IN_PLAY_NEXT);
-			return true;
-		}
-		return false;
+		return mouseButtonUpHandlers.find(areaName)->second();
 	}
 
 	void Start()
