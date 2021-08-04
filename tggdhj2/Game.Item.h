5a013ee95641ea196dbbae173b657eb4ab63250b
ernestpazera <ernestpazera@gmail.com>
2021-08-04 05:55:36 -0500

    new item ideas stubbed out

--- tggdhj2/Game.Item.h
+++ tggdhj2/Game.Item.h
@@ -5,10 +5,14 @@ namespace game
 	{
 		TROUSERS,
 		CODPIECE,
-		WATERSKIN,
 		COMPASS,
 		TRANSFUNCTIONER,
 		YER_CAR,
-		FOUNTAIN
+		FOUNTAIN,
+		ZOLTAN,//TODO: ????
+		HOT_CHICKS,//TODO: wants transfunctioner, but if given it, game ends, death by giant bikini woman
+		MACGUFFIN,//TODO: its a thing. that you carry around. that's it. WHAT?
+		CHINESE_FOOD_LADY,//TODO: interacting causes the game to go into an infinite "AND THEN?" loop
+		NORDIC_GUYS //TODO:wants transfunctioner, ???
 	};
 }
