9b74189ae329ced7bd86d3cb7e0964d1a34aaf52
ernestpazera <ernestpazera@gmail.com>
2021-08-06 04:19:19 -0500

    confidence and dignity on final screens

--- tggdhj2/State.InPlay.Lose.cpp
+++ tggdhj2/State.InPlay.Lose.cpp
@@ -3,10 +3,16 @@
 #include "Application.MouseButtonUp.h"
 #include "Application.OnEnter.h"
 #include "Application.UIState.h"
+#include <format>
 #include "Game.Audio.Mux.h"
+#include "Game.Avatar.Statistics.h"
+#include "Game.Statistics.h"
+#include "Visuals.Texts.h"
 namespace state::in_play::Lose
 {
 	const std::string LAYOUT_NAME = "State.InPlay.Lose";
+	const std::string TEXT_CONFIDENCE = "Confidence";
+	const std::string TEXT_DIGNITY = "Dignity";
 
 	static bool OnMouseButtonUp(const common::XY<int>& xy, MouseButton)
 	{
@@ -14,9 +20,18 @@ namespace state::in_play::Lose
 		return true;
 	}
 
+	static void OnEnter()
+	{
+		game::audio::Mux::Play(game::audio::Mux::Theme::MAIN);
+		auto descriptor = game::Statistics::Read(game::Statistic::CONFIDENCE);
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_CONFIDENCE, std::format(descriptor.format, game::avatar::Statistics::GetCurrent(game::Statistic::CONFIDENCE)));
+		descriptor = game::Statistics::Read(game::Statistic::DIGNITY);
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_DIGNITY, std::format(descriptor.format, game::avatar::Statistics::GetCurrent(game::Statistic::DIGNITY)));
+	}
+
 	void Start()
 	{
-		::application::OnEnter::AddHandler(::UIState::IN_PLAY_LOSE, game::audio::Mux::GoToTheme(game::audio::Mux::Theme::MAIN));
+		::application::OnEnter::AddHandler(::UIState::IN_PLAY_LOSE, OnEnter);
 		::application::MouseButtonUp::AddHandler(::UIState::IN_PLAY_LOSE, OnMouseButtonUp);
 		::application::Command::SetHandler(::UIState::IN_PLAY_LOSE, ::application::UIState::GoTo(::UIState::MAIN_MENU));
 		::application::Renderer::SetRenderLayout(::UIState::IN_PLAY_LOSE, LAYOUT_NAME);
--- tggdhj2/State.InPlay.Win.cpp
+++ tggdhj2/State.InPlay.Win.cpp
@@ -6,11 +6,15 @@
 #include <format>
 #include "Game.Audio.Mux.h"
 #include "Game.Avatar.h"
+#include "Game.Avatar.Statistics.h"
+#include "Game.Statistics.h"
 #include "Visuals.Texts.h"
 namespace state::in_play::Win
 {
 	const std::string LAYOUT_NAME = "State.InPlay.Win";
 	const std::string TEXT_EFFECTIVENESS = "Effectiveness";
+	const std::string TEXT_CONFIDENCE = "Confidence";
+	const std::string TEXT_DIGNITY = "Dignity";
 	const std::string FORMAT_EFFECTIVENESS = "Effectiveness: {:.2f}%";
 
 	static bool OnMouseButtonUp(const common::XY<int>& xy, MouseButton)
@@ -23,6 +27,10 @@ namespace state::in_play::Win
 	{
 		game::audio::Mux::Play(game::audio::Mux::Theme::MAIN);
 		visuals::Texts::SetText(LAYOUT_NAME, TEXT_EFFECTIVENESS, std::format(FORMAT_EFFECTIVENESS, game::Avatar::GetEffectiveness()));
+		auto descriptor = game::Statistics::Read(game::Statistic::CONFIDENCE);
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_CONFIDENCE, std::format(descriptor.format, game::avatar::Statistics::GetCurrent(game::Statistic::CONFIDENCE)));
+		descriptor = game::Statistics::Read(game::Statistic::DIGNITY);
+		visuals::Texts::SetText(LAYOUT_NAME, TEXT_DIGNITY, std::format(descriptor.format, game::avatar::Statistics::GetCurrent(game::Statistic::DIGNITY)));
 	}
 
 	void Start()
